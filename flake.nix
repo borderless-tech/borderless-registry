22069f9e19a6235dc98164e255808c0d2304e1f0
Paul Jökel <pbecker91@protonmail.com>
2025-06-22 23:55:39 +0200

    add basic project files

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "A rust devShell";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+    # add compatibility layer to generate shell.nix based on flakes
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            # Override the Rust toolchain to include llvm-tools-preview
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "llvm-tools-preview" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+            rustc
+            cargo
+            cargo-edit
+            cargo-audit
+            cargo-tarpaulin
+            cargo-flamegraph
+
+	    # Tools
+	    gh
+	    yamllint
+          ] ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            pkgs.libiconv
+          ];
+
+          # Certain Rust tools won't work without this
+          # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+          # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      }
+    );
+}
